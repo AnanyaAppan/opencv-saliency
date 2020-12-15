44d2adbf6bcc440ec12054c7dc1fc8a5d4ce9019
Ananya <ananya.appan@gmail.com>
2020-12-15 21:41:17 +0530

    changed to generate for images

--- run.sh
+++ run.sh
@@ -1,12 +1,5 @@
 
 for d in ../data/GO_PRO/test/*/sharp/*; do
   echo "$d"
-  if [ -d "$d" ]; then
-    echo $d
-    dir="$d/*.png"
-    for img in $dir; do
-        echo $img
-        python saliency_map.py --image $img
-    done
-  fi
+  python saliency_map.py --image $d
 done
